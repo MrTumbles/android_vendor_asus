243eeb4611c0e35430e61cd1baae0a527622a3a8
Mahdi-Rom <mahdi.rom@gmail.com>
2014-06-20 13:54:11 +0100

    Flo: 4.4.4 blobs from AOSPA
    Thanks to @linuxman94

--- flo/asus/asus.mk
+++ flo/asus/asus.mk
@@ -1,11 +1,10 @@
-# Copyright 2013 The Android Open Source Project
-# Copyright 2013 ParanoidAndroid Project
+# Copyright 2013 The ParanoidAndroid Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Call makefiles
-$(call inherit-product-if-exists, vendor/asus/flo/broadcom/broadcom.mk)
-$(call inherit-product-if-exists, vendor/asus/flo/asus/asus.mk)
-$(call inherit-product-if-exists, vendor/asus/flo/qcom/qcom.mk)
-$(call inherit-product-if-exists, vendor/asus/flo/others/others.mk)
+# Asus blob(s) necessary for Flo hardware
+PRODUCT_COPY_FILES := \
+    vendor/asus/flo/asus/libacdbdata.so:system/lib/libacdbdata.so:asus \
+    vendor/asus/flo/asus/libAKM.so:system/lib/libAKM.so:asus
+
